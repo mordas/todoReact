5bf9ab4ba7821836122c2923e0e63a1af8511bfb
dmitryMordas <dontbeatmybrain@gmail.com>
2018-12-12 15:48:06 +0300

    заготовка для todo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const TodoList = () => {
+        return (
+         <ul>
+                 <li>Learn React</li>
+                 <li>Build Awesome Ap</li>
+         </ul>
+        );
+};
+const AppHeader = () => {
+        return (
+         <h1>My Todo List</h1>
+        );
+};
+const SearchPanel = () => {
+        const searchStyle = {
+                fontSize: '25px'
+        }
+        return (
+        <input
+                style={searchStyle}
+                placeholder="search"
+                />
+        );
+};
+
+const App = () => {
+        return (
+        <div>
+         <AppHeader />
+         <SearchPanel />
+         <TodoList />
+ </div>
+        );
+};
+
+ReactDom.render(<App />,document.getElementById('root'));
+
